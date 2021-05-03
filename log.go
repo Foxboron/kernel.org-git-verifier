b4bcbfb36209196c52bde0ccfa9ca4366e3c593b
Morten Linderud <morten@linderud.pw>
2021-05-03 18:03:01 +0200

    Added logging
    Signed-off-by: Morten Linderud <morten@linderud.pw>

---
+++ log.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	// log.SetFormatter(&log.JSONFormatter{})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.DebugLevel)
+}
